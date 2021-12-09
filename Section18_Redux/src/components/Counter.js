39ce25addf788a5fcefdda4a4f7758116088c503
MStrong <59189994+mstrong-tech@users.noreply.github.com>
2021-12-09 10:33:39 +1000

    Redux guide

---
+++ Section18_Redux/src/components/Counter.js
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import { counterActions } from "../store/counter";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.value);
+  const show = useSelector((state) => state.counter.showCounter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>+ 1</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
+        <button onClick={decrementHandler}>- 1</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
---
+++ Section18_Redux/src/store/counter.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialCounterState = { value: 0, showCounter: true };
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialCounterState,
+    reducers: {
+        increment(state) {
+            state.value++;
+        },
+        decrement(state) {
+            state.value--;
+        },
+        increase(state, action) {
+            state.value = state.value + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
+
+export const counterActions = counterSlice.actions;
+
+export default counterSlice.reducer;
