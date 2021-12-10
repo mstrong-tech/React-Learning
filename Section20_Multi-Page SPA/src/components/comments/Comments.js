bfa2d2ee802c17c39811cd4bf2bb2f637525f262
MStrong <59189994+mstrong-tech@users.noreply.github.com>
2021-12-10 23:13:43 +1000

    Multi-Page SPA

---
+++ Section20_Multi-Page SPA/src/components/comments/Comments.js
@@ -0,0 +1,65 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import NewCommentForm from './NewCommentForm';
+import CommentsList from './CommentsList';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import classes from './Comments.module.css';
+
+const Comments = () => {
+  const [isAddingComment, setIsAddingComment] = useState(false);
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  const { quoteId } = useParams();
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
+
+  const startAddCommentHandler = () => {
+    setIsAddingComment(true);
+  };
+
+  const onAddedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if (status === 'pending') {
+    comments = (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (
+    status === 'completed' &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className='centered'>No comments were added yet!</p>;
+  }
+
+  return (
+    <section className={classes.comments}>
+      <h2>User Comments</h2>
+      {!isAddingComment && (
+        <button className='btn' onClick={startAddCommentHandler}>
+          Add a Comment
+        </button>
+      )}
+      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={onAddedCommentHandler} />}
+      {comments}
+    </section>
+  );
+};
+
+export default Comments;
