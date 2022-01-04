f9966c78790855018e2298912f5ed1ab26ca137f
MStrong <59189994+mstrong-tech@users.noreply.github.com>
2022-01-04 21:49:41 +1000

    Updated

--- Section29_React Summary/src/index.js
+++ Section29_React Summary/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { FavoritesContextProvider } from './store/favorites-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>,
+  document.getElementById('root')
+);
